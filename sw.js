3ba7f61a0455feb686261adab41590b3cc8e92be
Tiago de Carvalho Resende <tiago.dsn.erp@alterdata.matriz>
2017-05-17 10:23:07 -0300

    Test

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 this.addEventListener('install', (event) => {});
 
 this.addEventListener('fetch', (event) => {
-    if (new URL(event.request.url) === 'google.com') {
+    if (new URL(event.request.url) === 'https://tiagor87.github.io/teste') {
         event.respondWith(new Response('Teste'));
     }
 });
